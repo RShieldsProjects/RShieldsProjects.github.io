09f6b76c1c48765f1c824c698da1acdec3ba963e
RShields <playnow254@gmail.com>
2018-03-23 16:01:31 -0700

    Initial push

---
+++ depp/depp.js
@@ -0,0 +1,90 @@
+var orig = `Si, sabes que ya llevo un rato mirandote
+Tengo que bailar contigo hoy (D.Y.)
+Vi que tu mirada ya estaba llamandome
+Muestrame el camino que yo voy (Oh)
+
+Tu, tu eres el iman y yo soy el metal
+Me voy acercando y voy armando el plan
+Solo con pensarlo se acelera el pulso (Oh yeah)
+
+Ya, ya me esta gustando mas de lo normal
+Todos mis sentidos van pidiendo mas
+Esto hay que tomarlo sin ningun apuro
+
+Despacito
+Quiero respirar tu cuello despacito
+Deja que te diga cosas al oido
+Para que te acuerdes si no estas conmigo
+
+Despacito
+Quiero desnudarte a besos despacito
+Firmo en las paredes de tu laberinto
+Y hacer de tu cuerpo todo un manuscrito (sube, sube, sube)
+(Sube, sube)
+
+Quiero ver bailar tu pelo
+Quiero ser tu ritmo
+Que le ense~es a mi boca
+Tus lugares favoritos (favoritos, favoritos baby)
+
+Dejame sobrepasar tus zonas de peligro
+Hasta provocar tus gritos
+Y que olvides tu apellido (Diridiri, dirididi Daddy)
+
+Si te pido un beso ven damelo
+Yo se que estas pensandolo
+Llevo tiempo intentandolo
+Mami, esto es dando y dandolo
+Sabes que tu corazon conmigo te hace bom, bom
+Sabes que esa beba esta buscando de mi bom, bom
+Ven prueba de mi boca para ver como te sabe
+Quiero, quiero, quiero ver cuanto amor a ti te cabe
+Yo no tengo prisa, yo me quiero dar el viaje
+Empecemos lento, despues salvaje
+
+Pasito a pasito, suave suavecito
+Nos vamos pegando poquito a poquito
+Cuando tu me besas con esa destreza
+Veo que eres malicia con delicadeza
+
+Pasito a pasito, suave suavecito
+Nos vamos pegando, poquito a poquito
+Y es que esa belleza es un rompecabezas
+Pero pa montarlo aqui tengo la pieza
+
+Despacito
+Quiero respirar tu cuello despacito
+Deja que te diga cosas al oido
+Para que te acuerdes si no estas conmigo
+
+Despacito
+Quiero desnudarte a besos despacito
+Firmo en las paredes de tu laberinto
+Y hacer de tu cuerpo todo un manuscrito (sube, sube, sube)
+(Sube, sube)
+
+Quiero ver bailar tu pelo
+Quiero ser tu ritmo
+Que le ense~es a mi boca
+Tus lugares favoritos (favoritos, favoritos baby)
+
+Dejame sobrepasar tus zonas de peligro
+Hasta provocar tus gritos
+Y que olvides tu apellido
+
+Despacito
+Vamos a hacerlo en una playa en Puerto Rico
+Hasta que las olas griten "ay, bendito!"
+Para que mi sello se quede contigo
+
+Pasito a pasito, suave suavecito
+Nos vamos pegando, poquito a poquito
+Que le ense~es a mi boca
+Tus lugares favoritos (favoritos, favoritos baby)
+
+Pasito a pasito, suave suavecito
+Nos vamos pegando, poquito a poquito
+Hasta provocar tus gritos
+Y que olvides tu apellido (D.Y.)
+
+Despacito `;
---
+++ depp/index.js
@@ -0,0 +1,99 @@
+var comboConsonants = ['b','c','d','f','k','l','m','n','p','r','s','t','v','z'];
+var loneConsonants = comboConsonants.concat('g','h','j','x','y','bl','br','ch','cl','cr','dr','fl','fr','gl','gr','ll','pl','pr','rr','sc','scr','sh','sl','sn','sp','spl','spr','st','str','th','tr','~');
+var vowels = ['a','e','i','o','u'];
+var HTMLreplacements = [[/\n/g,'<br>'],[/~/g,'&ntilde;'],[/`/g,'&Ntilde;']];
+var odds = 1/10;
+
+function depp() {
+    odds = 1/document.getElementById("odds").value;
+    
+    var arr = orig.split('').reverse();
+    var output = '';
+    while (arr.length > 0) {
+        var word = '';
+        var letter = arr.pop();
+        while (/[a-zA-Z~]/.test(letter)) {
+            word += letter;
+            letter = arr.pop();
+        }
+        output += mutate(word);
+        output += letter;
+    }
+    document.getElementById("deppDiv").innerHTML = HTMLify(output);
+}
+
+function mutate(str) {
+    if (str == '') return '';
+    
+    let capitalize = /[A-Z~]/.test(str.charAt(0));
+    
+    if (str.toLowerCase() == 'y') {
+        chr = maybeSample(str, vowels);
+        if (capitalize) {
+            return chr.toUpperCase();
+        } else {
+            return chr;
+        }
+    }
+    
+    var arr = str.toLowerCase().split('').reverse();
+    var output = '';
+    
+    var chunk = '';
+    while (arr.length > 0) {
+        var letter = arr.pop();
+        if (vowels.indexOf(letter) != -1 && chunk.charAt(chunk.length - 1) != 'q') {
+            if (chunk != '') {
+                if (output == '' || Math.random() > 0.5) {
+                    output += maybeSample(chunk, loneConsonants);
+                } else {
+                    output += maybeSample(chunk, comboConsonants, 2);
+                }
+            }
+            chunk = '';
+            
+            output += maybeSample(letter, vowels);
+        } else {
+            chunk += letter;
+        }
+    }
+    output += maybeSample(chunk, comboConsonants);
+    
+    if (capitalize) {
+        return titleify(output);
+    } else {
+        return output;
+    }
+}
+
+function maybeSample(chunk, arr, number) {
+    if (chunk == '') return '';
+    if (typeof(number) === 'undefined') number = 1;
+    
+    if (Math.random() > odds) {
+        return chunk;
+    } else {
+        let output = '';
+        for (let i = 0; i < number; i++){
+            output += arr[Math.floor(Math.random() * arr.length)]
+        }
+        return output;
+    }
+}
+
+function titleify(str) {
+    var firstChar = str.charAt(0);
+    if (firstChar == '~') {
+        return '`' + str.slice(1);
+    } else {
+        return firstChar.toUpperCase() + str.slice(1);
+    }
+}
+
+function HTMLify(str) {
+    output = str;
+    for (let i = 0; i < HTMLreplacements.length; i++) {
+        output = output.replace(HTMLreplacements[i][0], HTMLreplacements[i][1]);
+    }
+    return output;
+}
