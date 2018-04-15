ff1c1286541f959a790751959ad1a1a27bd4bc56
RShields <playnow254@gmail.com>
2018-04-14 22:29:27 -0700

    Always butcher despacito?
    Always butcher despacito?
    Centered everything
    Added title of song
    Small changes that don't do anything (hopefully)

--- depp/index.js
+++ depp/index.js
@@ -1,11 +1,13 @@
 var comboConsonants = ['b','c','d','f','k','l','m','n','p','r','s','t','v','z'];
 var loneConsonants = comboConsonants.concat('g','h','j','x','y','bl','br','ch','cl','cr','dr','fl','fr','gl','gr','ll','pl','pr','rr','sc','scr','sh','sl','sn','sp','spl','spr','st','str','th','tr','~');
 var vowels = ['a','e','i','o','u'];
-var HTMLreplacements = [[/\n/g,'<br>'],[/~/g,'&ntilde;'],[/`/g,'&Ntilde;']];
-var odds = 1/10;
+var HTMLreplacements = [[/\n/g,'<br>'], [/~/g,'&ntilde;'], [/`/g,'&Ntilde;']];
+var odds = 1 / 10;
+var alwaysDespacito = false;
 
 function depp() {
-    odds = 1/document.getElementById("odds").value;
+    odds = 1 / document.getElementById("odds").value;
+    alwaysDespacito = document.getElementById("alwaysBox").checked;
     
     var arr = orig.split('').reverse();
     var output = '';
@@ -16,10 +18,13 @@ function depp() {
             word += letter;
             letter = arr.pop();
         }
+        
         output += mutate(word);
         output += letter;
     }
-    document.getElementById("deppDiv").innerHTML = HTMLify(output);
+    
+    let title = alwaysDespacito ? titleify(mutateDespacito()) : "Despacito";
+    document.getElementById("deppDiv").innerHTML = "<h1>" + title + "</h1><h2>Luis Fonsi ft. Daddy Yankee</h2><br>" + HTMLify(output);
 }
 
 function mutate(str) {
@@ -27,37 +32,34 @@ function mutate(str) {
     
     let capitalize = /[A-Z~]/.test(str.charAt(0));
     
-    if (str.toLowerCase() == 'y') {
-        chr = maybeSample(str, vowels);
-        if (capitalize) {
-            return chr.toUpperCase();
-        } else {
-            return chr;
-        }
-    }
-    
-    var arr = str.toLowerCase().split('').reverse();
     var output = '';
-    
-    var chunk = '';
-    while (arr.length > 0) {
-        var letter = arr.pop();
-        if (vowels.indexOf(letter) != -1 && chunk.charAt(chunk.length - 1) != 'q') {
-            if (chunk != '') {
-                if (output == '' || Math.random() > 0.5) {
-                    output += maybeSample(chunk, loneConsonants);
-                } else {
-                    output += maybeSample(chunk, comboConsonants, 2);
+    if (str.toLowerCase() == 'y') {
+        output = maybeSample(str, vowels);
+    } else if (alwaysDespacito && str.toLowerCase() == "despacito") {
+        output = mutateDespacito();
+    } else {
+        var arr = str.toLowerCase().split('').reverse();
+        
+        var chunk = '';
+        while (arr.length > 0) {
+            var letter = arr.pop();
+            if (vowels.indexOf(letter) != -1 && chunk.charAt(chunk.length - 1) != 'q') {
+                if (chunk != '') {
+                    if (output == '' || Math.random() > 0.5) {
+                        output += maybeSample(chunk, loneConsonants);
+                    } else {
+                        output += maybeSample(chunk, comboConsonants, 2);
+                    }
                 }
+                chunk = '';
+                
+                output += maybeSample(letter, vowels);
+            } else {
+                chunk += letter;
             }
-            chunk = '';
-            
-            output += maybeSample(letter, vowels);
-        } else {
-            chunk += letter;
         }
+        output += maybeSample(chunk, comboConsonants);
     }
-    output += maybeSample(chunk, comboConsonants);
     
     if (capitalize) {
         return titleify(output);
@@ -81,8 +83,22 @@ function maybeSample(chunk, arr, number) {
     }
 }
 
+function mutateDespacito() {
+    let origOdds = odds;
+    if (odds < 0.2) odds = 0.2;
+    alwaysDespacito = false;
+    
+    do {
+        var output = mutate("despacito");
+    } while (output == "despacito");
+    
+    odds = origOdds;
+    alwaysDespacito = true;
+    return output;
+}
+
 function titleify(str) {
-    var firstChar = str.charAt(0);
+    let firstChar = str.charAt(0);
     if (firstChar == '~') {
         return '`' + str.slice(1);
     } else {
