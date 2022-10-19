9d6c3ed278e4c4c6b960b7283ed5e79d43898968
RShields <playnow254@gmail.com>
2022-10-19 09:53:09 -0700

    Create midi2tmb

---
+++ projects/midi2tmb/index.js
@@ -0,0 +1,282 @@
+/** Data entry fields */
+var midiFileInput;
+var songNameInput;
+var shortNameInput;
+var artistInput;
+var releaseYearInput;
+var genreInput;
+var descriptionInput;
+var bpmInput;
+var beatsPerBarInput;
+var difficultyInput;
+var noteSpacingInput;
+var noteStartColorInput;
+var noteEndColorInput;
+var folderNameInput;
+var songEndpointInput;
+
+/** Warnings element */
+var warningsDiv;
+/** Warnings, in the form { message: {data: ...} } */
+let warnings = {};
+
+/** Returns whether all required fields are filled in */
+function verifyInputs() {
+    const requiredFields = [
+        midiFileInput,
+        songNameInput,
+        shortNameInput,
+        artistInput,
+        releaseYearInput,
+        genreInput,
+        descriptionInput,
+        bpmInput,
+        beatsPerBarInput,
+        difficultyInput,
+        noteSpacingInput,
+        noteStartColorInput,
+        noteEndColorInput,
+        // We're okay with these being empty
+        // folderNameInput,
+        // songEndpointInput,
+    ];
+
+    for (const field of requiredFields) {
+        if (!field.value) return false;
+    }
+
+    return true;
+}
+
+/** Entrypoint: read the midi, generate the tmb, and save it */
+async function generate() {
+    warnings = {};
+    warningsDiv.innerHTML = '';
+
+    if (!verifyInputs()) {
+        alert('Please fill out all fields\n(Song Endpoint and Folder Name can be empty)');
+        return;
+    }
+
+    const bpm = Number(bpmInput.value);
+
+    const midi = MidiParser.parse(await readFileToBytes());
+    const { notes, calculatedEndpoint } = midiToNotes(midi);
+    const endpoint = songEndpointInput.value
+        ? Number(songEndpointInput.value)
+        : calculatedEndpoint;
+
+    const tmb = {
+        notes,
+        name: songNameInput.value,
+        shortName: shortNameInput.value,
+        trackRef: folderNameInput.value,
+        year: Number(releaseYearInput.value),
+        author: artistInput.value,
+        genre: genreInput.value,
+        description: descriptionInput.value,
+        difficulty: Number(difficultyInput.value),
+        savednotespacing: Number(noteSpacingInput.value),
+        endpoint,
+        timesig: Number(beatsPerBarInput.value),
+        tempo: bpm,
+        lyrics: [],
+        note_color_start: hexToFloats(noteStartColorInput.value),
+        note_color_end: hexToFloats(noteEndColorInput.value),
+        UNK1: 0,
+    }
+    exportWarnings(warnings);
+    save('song.tmb', stringifyWithRounding(tmb));
+}
+
+/** Convert the midi to the notes array */
+function midiToNotes(midi) {
+    /** Length of a note (in beats) that would otherwise have 0 length */
+    const defaultNoteLength = 0.2;
+
+    const notes = [];
+    let lastMeasure = 0;
+
+    /* Begin translated section */
+    const { timeDivision } = midi;
+
+    /** All the events in the midi file, sorted by time */
+    const allMidiEvents = [];
+    for (const track of midi.track) {
+        let currTime = 0;
+
+        for (const event of track.event) {
+            currTime += event.deltaTime;
+            allMidiEvents.push({ ...event, time: currTime });
+        }
+    }
+    allMidiEvents.sort(function(a, b) { return a.time - b.time });
+
+    /** Note that we're currently creating */
+    let currentNote;
+    /** Pitch of the most recent note-on event */
+    let lastPitch;
+
+    for (const event of allMidiEvents) {
+        if (event.type === 9) { // note on
+            const pitch = event.data[0];
+            if (pitch < 47 || pitch > 73) {
+                addWarning(
+                    'Pitch out of range',
+                    { pitch, measure: Math.floor(event.time / timeDivision) }
+                );
+            }
+            lastPitch = pitch;
+
+            if (currentNote) {
+                const { startTime, startPitch } = currentNote;
+
+                const length = event.time - startTime;
+                const endPitch = pitch;
+                const pitchDelta = endPitch - startPitch;
+
+                notes.push([
+                    startTime / timeDivision,
+                    length > 0 ? length / timeDivision : defaultNoteLength,
+                    (startPitch - 60) * 13.75,
+                    pitchDelta * 13.75,
+                    (endPitch - 60) * 13.75,
+                ]);
+            }
+
+            currentNote = {
+                startTime: event.time,
+                startPitch: pitch,
+            }
+        } else if (event.type === 8) { // note off
+            const pitch = event.data[0];
+
+            // We ignore note-off events for pitches other than the current one
+            // which prevents slide-start notes from ending slides
+            if (currentNote && pitch === currentNote.startPitch) {
+                const { startTime, startPitch } = currentNote;
+
+                const length = event.time - startTime;
+
+                notes.push([
+                    startTime / timeDivision,
+                    length > 0 ? length / timeDivision : defaultNoteLength,
+                    (startPitch - 60) * 13.75,
+                    0,
+                    (startPitch - 60) * 13.75,
+                ]);
+                lastMeasure = Math.ceil(event.time / timeDivision);
+
+                currentNote = undefined;
+            }
+        } else if (event.type === 255) { // meta
+            if (event.metaType === 81 && event.time !== 0) { // tempo change
+                addWarning(
+                    'Tempo change (unsupported)',
+                    { measure: Math.floor(event.time / timeDivision) }
+                );
+            }
+        }
+    }
+    /* End translated section */
+
+    return {
+        notes,
+        // It feels rushed to end immediately after the last note,
+        // so we add an extra 4 measures
+        calculatedEndpoint: lastMeasure + 4
+    };
+}
+
+/** Converts "#xxxxxx" to [float, float, float] */
+function hexToFloats(hexColor) {
+    const rInt = parseInt(hexColor.substring(1,3), 16);
+    const gInt = parseInt(hexColor.substring(3,5), 16);
+    const bInt = parseInt(hexColor.substring(5,7), 16);
+    return [rInt / 255, gInt / 255, bInt / 255];
+}
+
+/** Read the file into a Uint8Array */
+async function readFileToBytes() {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(midiFileInput.files[0]);
+    return new Promise(function(resolve) {
+        reader.addEventListener(
+            'load',
+            function(event) {
+                resolve(new Uint8Array(event.target.result));
+            },
+            { once: true }
+        );
+    });
+}
+
+/** Stringify some json but round all the floats to at most 3 decimals */
+function stringifyWithRounding(data) {
+    return JSON.stringify(data, function(key, value) {
+        if (typeof value === 'number') {
+            return Math.round(value * 1000) / 1000;
+        }
+        return value;
+    });
+}
+
+/**
+ * Downloads the data as a file
+ * https://stackoverflow.com/a/33542499
+ */
+function save(filename, data) {
+    const blob = new Blob([data], {type: 'application/json'});
+    if (window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    } else {
+        const elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;
+        document.body.appendChild(elem);
+        elem.click();
+        document.body.removeChild(elem);
+    }
+}
+
+/** Adds a warning to the warnings list */
+function addWarning(type, data) {
+    if (warnings[type] === undefined) warnings[type] = [];
+    warnings[type].push(data);
+}
+
+/** Prints the warnings list to the warningsDiv */
+function exportWarnings(warnings) {
+    for (const [warning, data] of Object.entries(warnings)) {
+        const elem = window.document.createElement('p');
+        // TODO sanitize
+        elem.innerHTML = `${warning}: ${JSON.stringify(data)}`;
+        warningsDiv.appendChild(elem);
+    }
+    if (Object.keys(warnings).length !== 0) {
+        alert('Created song.tmb with warnings. Check below the Generate button.');
+    }
+}
+
+/** Initializes the inputs and hooks */
+function init() {
+    midiFileInput = document.getElementById('midifile');
+    songNameInput = document.getElementById('songname');
+    shortNameInput = document.getElementById('shortname');
+    artistInput = document.getElementById('artist');
+    releaseYearInput = document.getElementById('releaseyear');
+    genreInput = document.getElementById('genre');
+    descriptionInput = document.getElementById('description');
+    bpmInput = document.getElementById('bpm');
+    beatsPerBarInput = document.getElementById('beatsperbar');
+    difficultyInput = document.getElementById('difficulty');
+    noteSpacingInput = document.getElementById('notespacing');
+    noteStartColorInput = document.getElementById('notestartcolor');
+    noteEndColorInput = document.getElementById('noteendcolor');
+    folderNameInput = document.getElementById('foldername');
+    songEndpointInput = document.getElementById('songendpoint');
+
+    warningsDiv = document.getElementById('warnings');
+
+    document.getElementById('generatetmb').addEventListener('click', generate);
+}
