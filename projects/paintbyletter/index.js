beab8be65380bb3635491b4633aed7c02f1e3ed9
RShields <playnow254@gmail.com>
2026-03-12 19:33:36 -0700

    Improve PaintByLetter
    Change the color quantizer and remove rgbquant

--- projects/paintbyletter/index.js
+++ projects/paintbyletter/index.js
@@ -15,7 +15,11 @@ let quality;
 let width = 200;
 let height = 300;
 
-const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXY';
+let highlightColor = '#00ff00';
+let highlightAlpha = 100;
+let lock = 2;
+
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 let palette;
 let colors;
 let progress;
@@ -29,6 +33,10 @@ let current;
 
 let cvs;
 let ctx;
+
+// TODO: lock UI
+// TODO: highlight UI
+// TODO: robot?
 
 function sizeCanvas() {
   const { width, height } = cvs.getBoundingClientRect();
@@ -50,11 +58,8 @@ function sizeCanvas() {
 function drawCanvas() {
   transformContext();
 
-  ctx.fillStyle = 'gray';
+  ctx.fillStyle = '#808080';
   ctx.fillRect(-9999, -9999, 19999, 19999);
-
-  ctx.fillStyle = 'gray';
-  ctx.fillRect(1, 1, 1, 1);
 
   if (!current) return;
 
@@ -91,6 +96,13 @@ function transformContext() {
 }
 
 function drawPixel(i, j) {
+  const scaleMulti = baseScale * Math.pow(scaleFactor, scale);
+  const screenCoordsIj = [(i + translateX) * scaleMulti, (j + translateY) * scaleMulti];
+  const screenCoordsIpoJpo = [(i + 1 + translateX) * scaleMulti, (j + 1 + translateY) * scaleMulti];
+  if (screenCoordsIj[0] > ctx.width || screenCoordsIj[1] > ctx.height || screenCoordsIpoJpo[0] < 0 || screenCoordsIpoJpo[1] < 0) {
+    return;
+  }
+
   const indexIj = i + j * width;
   const correctIj = correct[indexIj];
   const currentIj = current[indexIj];
@@ -101,8 +113,16 @@ function drawPixel(i, j) {
   }
 
   if (correctIj !== undefined && currentIj !== correctIj) {
+    const highlighted = highlightAlpha > 0 && correctIj === selected;
+    if (highlighted) {
+      ctx.fillStyle = highlightColor + ('0' + highlightAlpha.toString(16)).slice(-2);
+      ctx.fillRect(i, j, 1, 1);
+    }
+
     const chr = alphabet[correctIj];
-    if (currentIj !== -1 && lightness(palette[currentIj]) < 75) {
+    // #808080 = (128, 128, 128)
+    const lumaIj = lumaWithHighlight(currentIj === -1 ? [128, 128, 128] : palette[currentIj], highlighted);
+    if (lumaIj < 75) {
       ctx.fillStyle = '#bbb';
     } else {
       ctx.fillStyle = 'black';
@@ -112,8 +132,18 @@ function drawPixel(i, j) {
 }
 
 // https://stackoverflow.com/a/596243
-function lightness(color) {
+function lumaWithHighlight(baseColor, highlighted) {
+  const color = [...baseColor];
+  if (highlighted) {
+    color[0] = lerp(color[0], parseInt(highlightColor.slice(1, 3), 16), highlightAlpha / 255);
+    color[1] = lerp(color[1], parseInt(highlightColor.slice(3, 5), 16), highlightAlpha / 255);
+    color[2] = lerp(color[2], parseInt(highlightColor.slice(5, 7), 16), highlightAlpha / 255);
+  }
   return 0.2126 * color[0] + 0.7152 * color[1] + 0.0722 * color[2];
+}
+
+function lerp(a, b, t) {
+  return a * (1 - t) + b * t;
 }
 
 function stopEffects(e) {
@@ -183,6 +213,13 @@ function handleKeyPress(e) {
   selectColor(n);
 }
 
+function handlePaste(e) {
+  const files = (event.clipboardData || event.originalEvent.clipboardData).files;
+  if (files.length < 1) return;
+  stopEffects(e);
+  beginLoadImageFile(files[0]);
+}
+
 function paint(mouseX, mouseY) {
   if(!current) return;
 
@@ -192,12 +229,13 @@ function paint(mouseX, mouseY) {
   if (i < 0 || j < 0 || i >= width || j >= height) return;
   const index = i + j * width;
   if (current[index] === selected || correct[index] === undefined) return;
+  if (lock > 0 && current[index] === correct[index]) return;
+  if (lock === 2 && tool !== 'wand' && correct[index] !== selected) return;
 
   if (tool === 'pencil') {
     setPixel(i, j);
   } else if (tool === 'wand') {
-    selectColor(correct[index]);
-    setPixel(i, j);
+    setPixel(i, j, correct[index]);
   } else if (tool === 'bucket') {
     if (selected !== correct[index]) {
       setPixel(i, j);
@@ -223,12 +261,13 @@ function paint(mouseX, mouseY) {
   updateProgress();
 }
 
-function setPixel(i, j) {
+function setPixel(i, j, forceColor) {
+  const color = forceColor !== undefined ? forceColor : selected;
   if (!current) return;
   if (i < 0 || j < 0 || i >= width || j >= height) return;
   const index = i + j * width;
-  if (current[index] === selected || correct[index] === undefined) return;
-  current[index] = selected;
+  if (current[index] === color || correct[index] === undefined) return;
+  current[index] = color;
   drawPixel(i, j);
 }
 
@@ -245,6 +284,12 @@ function beginLoadImage(src) {
   colors.innerHTML = '';
 }
 
+function beginLoadImageFile(file) {
+  const fileReader = new FileReader();
+  fileReader.onload = (e) => beginLoadImage(e.target.result);
+  fileReader.readAsDataURL(file);
+}
+
 function completeLoadImage() {
   const maxPixels = baseMaxPixels * Math.pow(2, Number(quality.value));
   const pixelCount = tempImg.width * tempImg.height;
@@ -259,11 +304,14 @@ function completeLoadImage() {
   //imgCtx.imageSmoothingEnabled = false;
   tempImgCtx.drawImage(tempImg, 0, 0, width, height);
 
-  const q = new RgbQuant({ colors: alphabet.length, method: 1 });
-  q.sample(tempImgCtx);
-  palette = q.palette(true);
+  //const q = new RgbQuant({ colors: alphabet.length, method: 1 });
+  //q.sample(tempImgCtx);
+  //palette = q.palette(true);
+  const kmeansResult = kmeans(tempImgCtx.getImageData(0, 0, width, height).data);
+  palette = kmeansResult.palette;
 
-  correct = q.reduce(tempImgCtx, 2);
+  //correct = q.reduce(tempImgCtx, 2);
+  correct = kmeansResult.indices;
   current = correct.map(() => -1);
   drawCanvas();
 
@@ -277,6 +325,155 @@ function completeLoadImage() {
   }
 
   selectColor(0);
+}
+
+function kmeans(pixels) {
+  const length = pixels.length / 4;
+
+  let palette = [];
+  for (let i = 0; i < alphabet.length; i++) {
+    pixelIndex = Math.floor(Math.random() * length);
+    palette.push([...pixels.slice(pixelIndex * 4, pixelIndex * 4 + 3)]);
+  }
+
+  let runsLeft = 100;
+  while (runsLeft > 0) {
+    runsLeft--;
+
+    const sums = palette.map(() => [0, 0, 0]);
+    const counts = palette.map(() => 0);
+    for (let i = 0; i < length; i++) {
+      if (pixels[i * 4 + 3] < 128) continue;
+      const pixel = pixels.slice(i * 4, i * 4 + 3);
+      const minIndex = minIndexBy(palette, (c) => pixelDist(c, pixel));
+      sums[minIndex][0] += pixel[0];
+      sums[minIndex][1] += pixel[1];
+      sums[minIndex][2] += pixel[2];
+      counts[minIndex]++;
+    }
+
+    for (let i = 0; i < sums.length; i++) {
+      if (counts[i] === 0) {
+        pixelIndex = Math.floor(Math.random() * length);
+        sums[i] = [...pixels.slice(pixelIndex * 4, pixelIndex * 4 + 3)];
+      } else {
+        sums[i][0] /= counts[i];
+        sums[i][1] /= counts[i];
+        sums[i][2] /= counts[i];
+      }
+    }
+
+    let changed = false;
+    for (let i = 0; i < sums.length; i++) {
+      if (sums[i][0] !== palette[i][0]) changed = true;
+      if (sums[i][1] !== palette[i][1]) changed = true;
+      if (sums[i][2] !== palette[i][2]) changed = true;
+      if (changed) break;
+    }
+    if (!changed) break;
+
+    palette = sums;
+  }
+  console.log(runsLeft);
+
+  palette = palette.map((color) => color.map(Math.floor));
+  palette.sort(compareColors);
+
+  const indices = [];
+  for (let i = 0; i < length; i++) {
+    if (pixels[i * 4 + 3] < 128) {
+      indices.push(undefined);
+      continue;
+    }
+
+    const pixel = pixels.slice(i * 4, i * 4 + 3);
+    const minIndex = minIndexBy(palette, (c) => pixelDist(c, pixel));
+    indices.push(minIndex);
+  }
+
+  return { palette, indices };
+}
+
+function minIndexBy(arr, f) {
+  let minVal = Infinity;
+  let minIndex;
+  for (let i = 0; i < arr.length; i++) {
+    const val = f(arr[i]);
+    if (val < minVal) {
+      minVal = val;
+      minIndex = i;
+    }
+  }
+  return minIndex;
+}
+
+function pixelDist(a, b) {
+  const d0 = a[0] - b[0];
+  const d1 = a[1] - b[1];
+  const d2 = a[2] - b[2];
+  return d0 * d0 + d1 * d1 + d2 * d2;
+}
+
+function compareColors(a, b) {
+  const hslA = hueSatLum(a);
+  const hslB = hueSatLum(b);
+  
+  const dHue = hslB.hue - hslA.hue;
+  const dLuma = hslB.luma - hslA.luma;
+  const dSat = hslB.sat - hslA.sat;
+
+  if (hslA.luma < 0.1 && hslB.luma > 0.1) return 1;
+  if (hslB.luma < 0.1 && hslA.luma > 0.1) return -1;
+
+  if (hslA.luma > 0.9 && hslB.luma < 0.9) return -1;
+  if (hslB.luma > 0.9 && hslA.luma < 0.9) return 1;
+
+  if (hslA.luma < 0.1 || hslA.luma > 0.9) return dLuma;
+
+  return 4 * dHue + 2 * dLuma + dSat;
+}
+
+// 0 to 1
+function hueSatLum(color) {
+  let [r, g, b] = color;
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  const max = Math.max(r,g,b);
+  const min = Math.min(r,g,b);
+  const range = max - min;
+  
+  let hue;
+  if (range === 0) {
+    hue = 0;
+  } else if (max === r) {
+    hue = ((g - b) / range + 6) % 6
+  } else if (max === g) {
+    hue = (b - r) / range + 2
+  } else if (max === b) {
+    hue = (r - g) / range + 4
+  }
+  hue /= 6;
+
+  const lum = (max + min) / 2;
+
+  const luma = 0.2627 * r + 0.6780 * g + 0.0593 * b;
+  
+  let sat;
+  if (lum < 0.5) {
+    sat = range / max;
+  } else {
+    sat = range / (1 - Math.abs(2 * lum - 1));
+  }
+
+  return { hue, sat, lum, luma };
+}
+
+function clamp(x, min, max) {
+  if (x < min) return min;
+  if (x > max) return max;
+  return x;
 }
 
 function selectTool(newTool) {
@@ -297,6 +494,7 @@ function selectColor(n) {
   selected = n;
   colors.children[n].classList.add('selected');
   updateProgress();
+  if (highlightAlpha) drawCanvas();
 }
 
 function updateProgress() {
@@ -315,7 +513,7 @@ function updateProgress() {
     if (current[i] === correct[i]) totalCorrect++;
   }
 
-progress.innerText = `${alphabet[selected]}:\n${selectedCorrect}/${selectedTotal} (${Math.floor(100 * selectedCorrect / selectedTotal)}%)\nTotal:\n${totalCorrect}/${totalTotal} (${Math.floor(100 * totalCorrect / totalTotal)}%)`;
+  progress.innerText = `${alphabet[selected]}:\n${selectedCorrect}/${selectedTotal} (${Math.floor(100 * selectedCorrect / selectedTotal)}%)\nTotal:\n${totalCorrect}/${totalTotal} (${Math.floor(100 * totalCorrect / totalTotal)}%)`;
 }
 
 window.addEventListener('load', function () {
@@ -330,9 +528,7 @@ window.addEventListener('load', function () {
     beginLoadImage(loadUrl.value);
   });
   loadFile = document.getElementById('loadfile');
-  loadFile.addEventListener('change', function() {
-    beginLoadImage(URL.createObjectURL(loadFile.files[0]));
-  });
+  loadFile.addEventListener('change', () => { beginLoadImageFile(loadFile.files[0]) });
 
   toolButtons['pencil'] = document.getElementById('pencil');
   toolButtons['bucket'] = document.getElementById('bucket');
@@ -347,6 +543,7 @@ window.addEventListener('load', function () {
   document.addEventListener('mouseup', handleMouseUp);
   cvs.addEventListener('mousemove', handleDrag);
   document.addEventListener('contextmenu', stopEffects);
+  document.addEventListener('paste', handlePaste);
 
   sizeCanvas();
   drawCanvas();
